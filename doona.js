a05fb84c57202f6e20beeb913d894347aa315fd1
xodnd0507 <24457545yong@gmail.com>
2023-10-11 16:56:41 +0900

    Chore : 디자인 변경

--- doona.js
+++ doona.js
@@ -1,7 +1,7 @@
 $(function(){  //코드를 바깥쪽에 적어서 제이쿼리 실행
 
     //버튼클릭시 동작하는 코드
-    $("button").click(function(){ //클릭했을 때 어떤일이 일어날지 (button은 html의 버튼태그다.)
+    $(".btn-8").click(function(){ //클릭했을 때 어떤일이 일어날지 (button은 html의 버튼태그다.)
       $(".modal").fadeIn();//페이드인으로 모달창을 보이게 해줌
     });
     
@@ -10,4 +10,16 @@ $(function(){  //코드를 바깥쪽에 적어서 제이쿼리 실행
       $(".modal").fadeOut();//클릭했을때 사라지게 하는 코드 페이드아웃을 이용함
     });
     
+    //버튼클릭시 동작하는 코드
+    $(".btn-9").click(function(){ //클릭했을 때 어떤일이 일어날지 (button은 html의 버튼태그다.)
+        $(".modal2").fadeIn();//페이드인으로 모달창을 보이게 해줌
+      });
+      
+      //모달창 클릭시 동작하는 코드
+      $(".modal_content2").click(function(){ //class로 선언해둔 modal_content안의 영역을 클릭하면 
+        $(".modal2").fadeOut();//클릭했을때 사라지게 하는 코드 페이드아웃을 이용함
+      });
+    
   });
+
+  
--- index.js
+++ index.js
@@ -29,6 +29,13 @@ $(document).ready(function(){
           });
         });
     
-        
+        // 이미지슬라이드 페이드 인 페이드 아웃
+  $('.imgSlide a:gt(0)').hide();
+
+  setInterval(function(){
+    $('.imgSlide a:first-child').fadeOut()
+    .next('a').fadeIn()
+    .end().appendTo('.imgSlide');
+  }, 3000);
 
     });
